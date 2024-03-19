1e80d1d0e7f4a2bc6d4b1ec7cfaa5cbea440ba01
Jainam <49641518+Psyborgs-git@users.noreply.github.com>
2024-03-20 01:56:38 +0530

    Refactor productionEnv.js to use EnvironmentBuilder

--- dist/productionEnv.js
+++ dist/productionEnv.js
@@ -5,8 +5,8 @@ var react_relay_network_modern_1 = require("react-relay-network-modern");
 var middleware_1 = require("./middleware");
 var source = new relay_runtime_1.RecordSource();
 var store = new relay_runtime_1.Store(source);
-var prodEnv = function (props) { return new relay_runtime_1.Environment({
+var EnvironmentBuilder = function (props) { return new relay_runtime_1.Environment({
     network: new react_relay_network_modern_1.RelayNetworkLayer((0, middleware_1.middlewares)(props), {}),
     store: store
 }); };
-exports.default = prodEnv;
+exports.default = EnvironmentBuilder;
