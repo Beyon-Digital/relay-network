138fdddf3cfd454267d1b3922ff83c52bd9998d6
Jainam <49641518+Psyborgs-git@users.noreply.github.com>
2024-03-20 01:56:03 +0530

    Refactor productionEnv.ts to use EnvironmentBuilder

--- src/productionEnv.ts
+++ src/productionEnv.ts
@@ -4,10 +4,10 @@ import { MiddlewareBuilderProps, middlewares } from './middleware';
 
 const source = new RecordSource();
 const store = new Store(source);
-const prodEnv = (props: MiddlewareBuilderProps): Environment => new Environment({
+const EnvironmentBuilder = (props: MiddlewareBuilderProps): Environment => new Environment({
     network: new RelayNetworkLayer(middlewares(props), {}),
     store
 });
 
 
-export default prodEnv;
+export default EnvironmentBuilder;
